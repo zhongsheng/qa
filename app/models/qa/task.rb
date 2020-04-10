c66f6f5285c15a52e110d9867f38ee09c2bfae6d
zhong sheng <nily.zhong@gmail.com>
2020-04-10 17:22:18 +0800

    v7

--- app/models/qa/task.rb
+++ app/models/qa/task.rb
@@ -3,6 +3,8 @@ module Qa
     # belongs_to :qa_theme
     has_many :talks
 
+    validates :name, presence: true
+
     def talk_contents
       talks.map { |talk| talk.content_modifier }.join.html_safe
     end
--- app/models/qa/theme.rb
+++ app/models/qa/theme.rb
@@ -1,5 +1,6 @@
 module Qa
   class Theme < ApplicationRecord
     has_many :tasks
+    validates :name, presence: true
   end
 end
